3ac57ef07240bf904abffea4d42abe9c2763140c
Igor Kovatsenko <igor.kovatsenko@yandex.ru>
2019-04-10 13:25:41 +0400

    мое расписание

--- main.js
+++ main.js
@@ -1,144 +1,360 @@
-webix.ready(function () {
+webix.ready(function() {
+    render(new components());
+});
+
+function render(components) {
     webix.ui({
         type: "wide",
         cols: [
             {
+                id: "control",
+                rows: components.controlPanel(),
+            },
+            {
+                id: "control_menu_1_1_data",
+                container: "",
+                hidden: false,
+                // контент расписания
                 rows: [
-                    {
-                        template: "account",
-                        height: 80
+                    { 
+                        template: "Мое расписание", 
+                        height: 40,
                     },
+                    { 
+                        template: "промежутки", 
+                        height: 40,
+                    },
+                    { 
+                        view:"scrollview", 
+                        id:"scrollview", 
+                        scroll:"x", 
+                        body:{
+                            cols: components.schedule("self") 
+                        }
+                    },
+                ]
+            },
+            {
+                id: "control_menu_2_data",
+                hidden: true,
+                // контент редактирования
+                template: "редактирование",
+            },
+            {
+                id: "control_menu_3_data",
+                hidden: true,
+                // контент настроек
+                template: "Настройки",
+            },
+            {
+                id: "control_menu_4_data",
+                hidden: true,
+                // контент помощи
+                template: "Помощь",
+            },
+            {
+                id: "control_menu_5_data",
+                hidden: true,
+                template: "Выход",
+            },
+        ],
+    });
+}
+
+function components() {
+    this.controlPanel = function () {
+        var currentContent = "control_menu_1_1_data";
+
+        return controlPanel = [
+            {
+                template: "account",
+                height: 80
+            },
+            {
+                view: "menu",
+                id: "control_menu",
+                layout: "y",
+                width: 200,
+                subMenuPos: "right",
+                select: false,
+                data: [
                     {
-                        view: "menu",
-                        id: "control_menu",
-                        layout: "y",
-                        width: 200,
-                        subMenuPos:"right",
-                        select: true,
-                        data: [{
-                                id: "1",
-                                value: "Расписание",
-                                data: {
-                                    container: "box",
-                                    height: 300,
-                                    width: 200,
-                                    view: "tree",
-                                    select: true,
-                                    data: schedulesData,
+                        id: "control_menu_1",
+                        value: "Расписание",
+                        data: {
+                            container: "box",
+                            height: 300,
+                            width: 200,
+                            view: "tree",
+                            data: [
+                                {
+                                    id: "control_menu_1_1",
+                                    value: "Мое расписание"
                                 },
-                            },
-                            {
-                                id: "2",
-                                value: "Редактировать",
-                                data: {
-                                    container: "box",
-                                    height: 300,
-                                    width: 200,
-                                    view: "tree",
-                                    select: true,
-                                    data: myschedulesData,
+                                {
+                                    id: "control_menu_1_2",
+                                    open: false,
+                                    value: "Группа 1",
+                                    data: [{
+                                            id: "control_menu_1_2_1",
+                                            value: "Сотрудник 1"
+                                        },
+                                        {
+                                            id: "control_menu_1_2_2",
+                                            value: "Сотрудник 2"
+                                        },
+                                        {
+                                            id: "control_menu_1_2_3",
+                                            value: "Сотрудник 3"
+                                        }
+                                    ],
                                 },
-                            },
-                            {
-                                $template: "Separator"
-                            },
-                            {
-                                id: "3",
-                                value: "Настройки"
-                            },
-                            {
-                                id: "4",
-                                value: "Помощь"
-                            },
-                            {
-                                id: "5",
-                                value: "Выход"
-                            }
-                        ],
-                        on: {
-                            onMenuItemClick: function (id) {
-                                $$("t1").setHTML(this.getMenuItem(id).value);
-                            }
+                                {
+                                    id: "control_menu_1_3",
+                                    value: "Группа 2",
+                                    data: [{
+                                            id: "control_menu_1_3_1",
+                                            value: "Сотрудник 4"
+                                        },
+                                        {
+                                            id: "control_menu_1_3_2",
+                                            value: "Сотрудник 5"
+                                        },
+                                        {
+                                            id: "control_menu_1_3_3",
+                                            value: "Сотрудник 6"
+                                        }
+                                    ]
+                                }
+                            ],
                         },
                     },
-                ]
+                    {
+                        id: "control_menu_2",
+                        value: "Редактирование",
+                        data: {
+                            container: "box",
+                            height: 300,
+                            width: 200,
+                            view: "tree",
+                            select: true,
+                            data: [
+                                {
+                                    id: "control_menu_2_1",
+                                    value: "Мое расписание"
+                                },
+                                {
+                                    id: "control_menu_2_2",
+                                    open: false,
+                                    value: "Шаблоны",
+                                    data: [{
+                                        id: "control_menu_2_2_1",
+                                        value: "Шаблон 1"
+                                    }]
+                                }
+                            ],
+                        },
+                    },
+                    {
+                        $template: "Separator"
+                    },
+                    {
+                        id: "control_menu_3",
+                        value: "Настройки",
+                        
+                    },
+                    {
+                        id: "control_menu_4",
+                        value: "Помощь"
+                    },
+                    {
+                        id: "control_menu_5",
+                        value: "Выход"
+                    },
+                ],
+                on:{
+                    onMenuItemClick:function(id){
+                        if ($$(id+"_data") != undefined) {
+                            console.log(id);
+                            $$(currentContent).hide();
+                            $$(id+"_data").show();
+                            currentContent = id+"_data";
+                        }
+                    }
+                },
+                type:{
+                    subsign:true
+                }
             },
-            {
-                view: "resizer",
-            },
-            {
-                id: "t1",
-                rows:[
-                    { template: "промежуток", height:40 },
-                    { 
-                        height:40,
-                        cols: [
-                            { template: "день" },
-                            { template: "день" },
-                            { template: "день" },
-                            { template: "день" },
-                            { template: "день" },
+        ];
+    };
+
+    /**
+     * расписание - массив таблиц(для каждого дня своя таблица),
+     */
+    this.schedule = function (key) {
+        switch (key) {
+            case "self":
+                return [
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"10.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
                         ]
                     },
-                    { template: "расписание" },
-                ]
-            },
-        ]
-    });
-});
-
-var schedulesData = [{
-        id: "schedulesData_1",
-        value: "Мое расписание"
-    },
-    {
-        id: "schedulesData_2",
-        open: false,
-        value: "Группа 1",
-        data: [{
-                id: "schedulesData_2.1",
-                value: "Сотрудник 1"
-            },
-            {
-                id: "schedulesData_2.2",
-                value: "Сотрудник 2"
-            },
-            {
-                id: "schedulesData_2.3",
-                value: "Сотрудник 3"
-            }
-        ]
-    },
-    {
-        id: "schedulesData_3",
-        value: "Группа 2",
-        data: [{
-                id: "schedulesData_3.1",
-                value: "Сотрудник 4"
-            },
-            {
-                id: "schedulesData_3.2",
-                value: "Сотрудник 5"
-            },
-            {
-                id: "schedulesData_3.3",
-                value: "Сотрудник 6"
-            }
-        ]
+                    {
+                        view:"datatable",
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"11.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"12.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"15.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-11", width:100 },
+                            { schedule:"14-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"16.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"17.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"18.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"19.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable",
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"22.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"23.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable",
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"24.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"25.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"26.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                    {
+                        view:"datatable", 
+                        scroll: false,
+                        autowidth: true,
+                        columns:[
+                            { id:"schedule", header:"27.04.2019", width:100 },
+                        ],
+                        data: [
+                            { schedule:"9-18", width:100 },
+                        ]
+                    },
+                ];
+        
+            default: break;
+        }
+            
     }
-]
-
-var myschedulesData = [{
-        id: "myschedulesData_1",
-        value: "Мое расписание"
-    },
-    {
-        id: "myschedulesData_2",
-        open: false,
-        value: "Шаблоны",
-        data: [{
-            id: "myschedulesData_2.1",
-            value: "Шаблон 1"
-        }]
-    }
-]
+}
