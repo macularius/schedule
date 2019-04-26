ec09ef4a43621d146133d7eed9e95c05ae65c750
Igor Kovatsenko <igor.kovatsenko@yandex.ru>
2019-04-26 11:42:25 +0400

    shedule provider init

--- application/providers/sheduleProvider.ts
+++ application/providers/sheduleProvider.ts
@@ -1,7 +1,25 @@
-import { iProvider } from "./iProvider";
+import { Provider } from "./provider";
+import { EmployTimetable } from "../entity/employTimetable";
+import { Employ } from "../entity/employ";
+import { Timetable } from "../entity/timetable";
+import { Day } from "../entity/day";
+import { TimeRange } from "../entity/timeRange";
 
-export class SheduleProvider implements iProvider {
+export class SheduleProvider extends Provider {
     load() {
+        return new EmployTimetable(
+            new Employ(1, "Коваценко", "Игорь", "Николаевич", "23.07.1998", "position", "email", "phone number"),
+            new Timetable([
+                new Day("Wed Apr 10 2019 00:00:00 GMT+0400 (GMT+04:00)", [new TimeRange("9", "18")]),
+                new Day("Thu Apr 11 2019 00:00:00 GMT+0400 (GMT+04:00)", [new TimeRange("9", "18")]),
+                new Day("Fri Apr 12 2019 00:00:00 GMT+0400 (GMT+04:00)", [new TimeRange("9", "18")]),
+                new Day("Mon Apr 15 2019 00:00:00 GMT+0400 (GMT+04:00)", [new TimeRange("9", "18")]),
+                new Day("Tue Apr 16 2019 00:00:00 GMT+0400 (GMT+04:00)", [new TimeRange("9", "18")]),
+                new Day("Wed Apr 17 2019 00:00:00 GMT+0400 (GMT+04:00)", [new TimeRange("9", "18")]),
+                new Day("Thu Apr 18 2019 00:00:00 GMT+0400 (GMT+04:00)", [new TimeRange("9", "18")]),
+                new Day("Fri Apr 19 2019 00:00:00 GMT+0400 (GMT+04:00)", [new TimeRange("9", "18")]),
+            ])
+        );
     }
     update() {
     }
