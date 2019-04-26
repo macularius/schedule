91da135abd649b1fea1df2565ef51aacc870d711
Igor Kovatsenko <igor.kovatsenko@yandex.ru>
2019-04-26 11:30:40 +0400

    menu provider fix

--- application/providers/menuProvider.ts
+++ application/providers/menuProvider.ts
@@ -2,11 +2,9 @@ import { Provider } from "./provider";
 import { Group } from "../entity/group";
 import { Employ } from "../entity/employ";
 
-export class MenuProvider implements Provider {
-    private data: Group[];
-
+export class MenuProvider extends Provider {
     constructor(){
-        this.data = [
+        super([
             new Group("1", [
                 new Employ(1, "Иван", "Иванов", "Иванович", "birthday", "position", "email", "phone number"),
                 new Employ(2, "Петр", "Петров", "Петрович", "birthday", "position", "email", "phone number"),
@@ -15,13 +13,13 @@ export class MenuProvider implements Provider {
                 new Employ(3, "Сидор", "Сидоров", "Сидорович", "birthday", "position", "email", "phone number"),
                 new Employ(2, "Федор", "Федоров", "Федорович", "birthday", "position", "email", "phone number"),
             ]),
-        ];
+        ]);
     }
 
     /**
      * возвращает группы с инициалами сотрудников
      */
-    load(): Array<object> {
+    load(): object[] {
         return this.data;
     }
     update(param: string): void {
