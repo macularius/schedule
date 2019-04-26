a5281945aa34347818aed47f4329505f90bb82e9
Igor Kovatsenko <igor.kovatsenko@yandex.ru>
2019-04-26 12:27:23 +0400

    component fix

--- application/kernel/component.ts
+++ application/kernel/component.ts
@@ -1,7 +1,14 @@
 import { iComponent } from "./iComponent";
 import { iListner } from "./iListner";
+import { iProvider } from "../providers/iProvider";
 
 export abstract class Component implements iComponent, iListner {
+    protected provider: iProvider;
+    
+    constructor(p: iProvider){
+        this.provider = p;
+    }
+
     // iListner
     handleEvent(e: string): void {
     }
